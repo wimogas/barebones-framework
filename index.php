4b8864a3cd4284445e59a8fa9f1fe1f127d519f5
Guillem Moya <wimogas@gmail.com>
2021-05-02 11:47:29 +0200

    minor fix

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Boilerplate Framework</title>
-    <script src=<?php echo $js_file ?> ></script>
+    <script src='<?php echo $js_file ?>'></script>
 </head>
 <body>
     

68767d0808536e9fa06829f3815b6a18e3e359fa
Guillem Moya <wimogas@gmail.com>
2022-02-12 10:28:20 +0100

    [SPRINT1.1] Add index page content and github link

--- index.php
+++ index.php
@@ -8,6 +8,31 @@
 <?php
     require_once 'inc/nav.inc'
 ?>
+<div class="wrapper">
+    <div class="container block direction-row justify-center pt-50">
+        <div class="block direction-column card w-50 align-center bg-white">
+            <h1 class="color-primary mb-10">Barebones</h1>
+            <p class="color-text mb-5">This is a simple but effective css framework</p>
+            <div class="block w-100">
+                <p class="color-text">CSS</p>
+            </div>
+            <div class="block mt-5">
+                <span class="code">
+                    &ltlink rel="stylesheet" href="https://barebones-framework.herokuapp.com/dist/barebones.css"&gt;<br>
+                </span>
+            </div>
+            <div class="block w-100 mt-5">
+                <p class="color-text">JS</p>
+            </div>
+            <div class="block mt-5">
+                <span class="code">
+                    &ltscript defer src="https://barebones-framework.herokuapp.com/dist/barebones.js"&gt;&ltscript/&gt;<br>
+                </span>
+            </div>
+        </div>
+    </div>
+</div>
+
 
 </body>
 </html>
